05b7e3a599a4635f39fb9e8a06f9b879bf901abe
Tony_Huang <luck938938@hotmail.com>
2019-06-17 21:45:30 -0400

    add schema

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema =require('./schema/schema')
 
+
 const app =express()
 
 app.use("/graphql", graphqlHTTP({
